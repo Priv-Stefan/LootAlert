d1f20005bab0846c0a6473c350a37ca4127dd56b
git config --global user.name Priv-Stefan <geissler.s@gmx.de>
2024-07-29 20:51:53 +0200

    Add Raid Warn with item link alert, Green underlay looted wish item

--- Lootalert.lua
+++ Lootalert.lua
@@ -24,8 +24,8 @@ end
 
 local function sort_comp(a,b)	
 	local name1,name2, link, quality, iLevel, reqLevel, class, subclass,maxStack, equipSlot, texture, vendorPrice 
-	name1, link, quality, iLevel, reqLevel, class, subclass,	maxStack, equipSlot, texture, vendorPrice = GetItemInfo(a['itemid'])
-	name2, link, quality, iLevel, reqLevel, class, subclass,	maxStack, equipSlot, texture, vendorPrice = GetItemInfo(b['itemid'])
+	name1, link, quality, iLevel, reqLevel, class, subclass,	maxStack, equipSlot, texture, vendorPrice = GetItemInfo(a['id'])
+	name2, link, quality, iLevel, reqLevel, class, subclass,	maxStack, equipSlot, texture, vendorPrice = GetItemInfo(b['id'])
 
 	return name1 < name2
 end
@@ -48,27 +48,15 @@ local function set_loot_slider_max()
 	end
 end
 
-local function show_loot()
-	if listlootedframes[1] == nil then
-		return
-	end
-	local index = 1
-	for i = 1, max_lines do
-		if i + loot_start_index - 1 <= #lola_variables.loot then
-			local name, link, quality, iLevel, reqLevel, class, subclass,
-			maxStack, equipSlot, texture, vendorPrice = GetItemInfo(lola_variables.loot[i + loot_start_index - 1]['id'])
-			listlootedframes[i].item:SetText( lola_variables.loot[i + loot_start_index - 1]['count'] .. " " .. link)
-			listlootedframes[i].link = link
-			listlootedframes[i]:Show()
-			listlootedframes[i].itemtexture:Show()
-			listlootedframes[i].itemtexture:SetTexture(texture)
-		else
-			listlootedframes[i].item:SetText("")
-			listlootedframes[i].itemtexture:Hide()
-			listlootedframes[i]:Hide()
+-- --------------------------------------------------------------------
+-- Check whether an ID is in a a list
+function isIDinList(id, list)
+	for i=1, #list do
+		if list[i]['id'] == id then
+			return true
 		end
 	end
-
+	return false
 end
 
 -- local function show_loot()
@@ -93,33 +81,38 @@ end
 -- 	end
 -- end
 
-
-local function scan()
-	debug("Scanning")
-	itemdb = nil
-	itemdb = {}
-	for i = 40000, 55000 do
-		local name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice =
-			GetItemInfo(i)
-		if name ~= nil then
-			if quality > 3 then
-				item = {}
-				item["name"] = name
-				item["id"] = i
-				table.insert(itemdb, item)
+-- --------------------------------------------------------
+-- Show looted items
+local function show_loot()
+	if listlootedframes[1] == nil then
+		return
+	end
+	local index = 1
+	for i = 1, max_lines do
+		if i + loot_start_index - 1 <= #lola_variables.loot then
+			local id = lola_variables.loot[i + loot_start_index - 1]['id']
+			local name, link, quality, iLevel, reqLevel, class, subclass,
+			maxStack, equipSlot, texture, vendorPrice = GetItemInfo( id )
+			listlootedframes[i].item:SetText( lola_variables.loot[i + loot_start_index - 1]['count'] .. " " .. link)
+			listlootedframes[i].link = link
+			listlootedframes[i]:Show()
+			listlootedframes[i].itemtexture:Show()
+			listlootedframes[i].itemtexture:SetTexture(texture)
+			if isIDinList(id, lola_variables.wishes) then
+				listlootedframes[i].background2:Show()
+			else
+				listlootedframes[i].background2:Hide()
 			end
+		else
+			listlootedframes[i].item:SetText("")
+			listlootedframes[i].itemtexture:Hide()
+			listlootedframes[i]:Hide()
 		end
 	end
-	if #itemdb > max_lines then
-		lola_looted.slider:SetMinMaxValues(1, #itemdb- max_lines + 1)
-	else
-		lola_looted.slider:SetMinMaxValues(1, 1)
-	end
-
-	show_loot()
-	debug("Scanning done")
 end
 
+-- --------------------------------------------------------
+-- Show wishlist
 local function show_items()
 	if listframes[1] == nil then
 		return
@@ -127,105 +120,29 @@ local function show_items()
 	local index = 1
 	for i = 1, max_lines do
 		if i + wish_start_index - 1 <= #lola_variables.wishes then
+			local id = lola_variables.wishes[i + wish_start_index - 1]["id"]
 			local name, link, quality, iLevel, reqLevel, class, subclass,
-			maxStack, equipSlot, texture, vendorPrice = GetItemInfo(lola_variables.wishes[i + wish_start_index - 1]["itemid"])
+			maxStack, equipSlot, texture, vendorPrice = GetItemInfo(id)
 			listframes[i].item:SetText(link)
 			listframes[i].link = link
 			listframes[i]:Show()
 			listframes[i].itemtexture:Show()
 			listframes[i].itemtexture:SetTexture(texture)
+			if isIDinList(id,lola_variables.loot) then
+				listframes[i].background2:Show()
+			else
+				listframes[i].background2:Hide()
+			end
 		else
 			listframes[i].item:SetText("")
 			listframes[i].itemtexture:Hide()
 			listframes[i]:Hide()
 		end
-	end
-end
-
-
-function lola_btnAdd_click(arg1)
-	local itemname = edtWish:GetText()
-	local name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice
-	local id = tonumber(itemname)
-	if id ~= nil then
-		name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice = GetItemInfo(
-			id)
-	else
-		name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice = GetItemInfo(
-			itemname)
-	end
-
-	if name == nil then
-		for i = 1, #itemdb do
-			if string.find(itemdb[i]['name'], itemname) then
-				name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice =
-					GetItemInfo(itemdb[i]['id'])
-				break;
-			end
-		end
-	end
-
-	if name ~= nil then
-		local itemNum = tonumber(link:match("|Hitem:(%d+):"))		
-		local w = {}
-		w['itemid'] = itemNum
-		table.insert(lola_variables.wishes, w)
-		table.sort(lola_variables.wishes,sort_comp)
-
-		if #lola_variables.wishes > max_lines then
-			set_slider_max()
-			wish_start_index = #lola_variables.wishes - max_lines + 1
-			lola_wish_frame.slider:SetValue(wish_start_index)
-		else
-			show_items()
-		end
-		edtWish:SetText("")
-	else
-		edtWish:SetText("nf " .. itemname)
-	end
-end
-
-function lola_btnscan_click()
-	scan()
-end
-
-function lola_btnDel_click(self)
-	local idx = tonumber(self:GetName():match('lolawish_(%d+)_'))
-	if idx ~= nil then 
-		table.remove(lola_variables.wishes, wish_start_index + idx - 1)
-		show_items()
-		set_slider_max()
-	else
-		idx = tonumber(self:GetName():match('lolaloot_(%d+)_'))		
-		if idx ~= nil then 
-			table.remove(lola_variables.loot, loot_start_index + idx - 1)
-			show_loot()
-			set_loot_slider_max()
-		end
-	end
-end
-
-local function load_variables()
-	LoLaMain:SetPoint("CENTER", "UIParent", "CENTER",
-		lola_variables.Xpos, lola_variables.Ypos)
-
-	--print("Loaded : ", lola_variables.Xpos, lola_variables.Ypos, lola_variables.wishes[1])
-
-	if lola_variables.wishes == nil then
-		lola_variables.wishes = {}
-	end
-
-	if lola_variables.loot == nil then
-		lola_variables.loot = {}
-	end
-	show_items()
-	set_slider_max()
-
-	show_loot()
-	set_loot_slider_max()
+	end	
 end
 
 -- -----------------------------------------------------------------
+-- Add looted item to the loot list
 local function add_loot( message )
 	local itemNum = tonumber(string.match(message,"item:(%d+):"))	
 
@@ -259,64 +176,195 @@ local function add_loot( message )
 				lola_looted.slider:SetValue(loot_start_index)
 			end
 			show_loot()
+			show_items() -- Color update
 		end
 	end
+end
+
+-- ----------------------------------------------------
+-- scan for items, so we can add them, even if they are 
+-- not in the char's item cache
+local function scan()
+	debug("Scanning")
+	itemdb = nil
+	itemdb = {}
+	for i = 40000, 55000 do
+		local name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice =
+			GetItemInfo(i)
+		if name ~= nil then
+			if quality > 3 then
+				item = {}
+				item["name"] = name
+				item["id"] = i
+				table.insert(itemdb, item)
+			end
+		end
+	end
+	if #itemdb > max_lines then
+		lola_looted.slider:SetMinMaxValues(1, #itemdb- max_lines + 1)
+	else
+		lola_looted.slider:SetMinMaxValues(1, 1)
+	end
+
+	show_loot()
+	debug("Scanning done")
+end
+
+
+-- ----------------------------------------------------
+-- Add item to the wishlist 
+function lola_btnAdd_click(arg1)
+	local itemname = edtWish:GetText()
+	local name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice
+	local id = tonumber(itemname)
+	if id ~= nil then
+		name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice = GetItemInfo(
+			id)
+	else
+		name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice = GetItemInfo(
+			itemname)
+	end
+
+	if name == nil then
+		for i = 1, #itemdb do
+			if string.find(itemdb[i]['name'], itemname) then
+				name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice =
+					GetItemInfo(itemdb[i]['id'])
+				break;
+			end
+		end
+	end
+
+	if name ~= nil then
+		local itemNum = tonumber(link:match("|Hitem:(%d+):"))		
+		local w = {}
+		w['id'] = itemNum
+		table.insert(lola_variables.wishes, w)
+		table.sort(lola_variables.wishes,sort_comp)
+
+		if #lola_variables.wishes > max_lines then
+			set_slider_max()
+			wish_start_index = #lola_variables.wishes - max_lines + 1
+			lola_wish_frame.slider:SetValue(wish_start_index)
+		else
+			show_items()
+		end
+		edtWish:SetText("")
+	else
+		edtWish:SetText("nf " .. itemname)
+	end
+end
+
+-- ----------------------------------------------------
+-- Delete item to the from lists
+function lola_btnDel_click(self)
+	local idx = tonumber(self:GetName():match('lolawish_(%d+)_'))
+	if idx ~= nil then 
+		table.remove(lola_variables.wishes, wish_start_index + idx - 1)
+		show_items()
+		set_slider_max()
+	else
+		idx = tonumber(self:GetName():match('lolaloot_(%d+)_'))		
+		if idx ~= nil then 
+			table.remove(lola_variables.loot, loot_start_index + idx - 1)
+			show_loot()
+			set_loot_slider_max()
+			show_items() -- Color update
+		end
+	end
+end
+
+function lola_btnscan_click()
+	scan()
 end
 
 local function warn_loot( message )
 	local itemNum = tonumber(string.match(message,"item:(%d+):"))
-
+	
 	if itemNum ~= nil then
-		local name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice= GetItemInfo(itemNum)
-		debug("Roll ", name)
+		if isIDinList(itemNum, lola_variables.wishes) then
+			PlaySoundFile("sound\\interface\\PVPFlagTakenHordeMono.wav")
+			local name, link, quality, iLevel, reqLevel, class, subclass, maxStack, equipSlot, texture, vendorPrice= GetItemInfo(itemNum)
+			debug("Roll ", name)
+		end
 	end
 end
+
 -- -----------------------------------------------------------------
-function lola_texture_OnEnter(self, motion)
-	
+local function load_variables()
+	LoLaMain:SetPoint("CENTER", "UIParent", "CENTER",
+		lola_variables.Xpos, lola_variables.Ypos)
+
+	if lola_variables.wishes == nil then
+		lola_variables.wishes = {}
+	else
+		-- compatibility for rename itemid to id 
+		for i=1,#lola_variables.wishes do
+			if lola_variables.wishes[i]['itemid'] ~= nil then
+				lola_variables.wishes[i]['id'] = lola_variables.wishes[i]['itemid']
+				lola_variables.wishes[i]['itemid'] = nil
+			end
+		end
+	end
+
+	if lola_variables.loot == nil then
+		lola_variables.loot = {}
+	end
+	show_items()
+	set_slider_max()
+
+	show_loot()
+	set_loot_slider_max()
+end
+
+-- -----------------------------------------------------------------
+-- Show the item tooltips
+function lola_texture_OnEnter(self, motion)	
 	local link = self:GetParent().link
 	if link then
 		GameTooltip:SetOwner(self, "ANCHOR_TOPRIGHT")
 		GameTooltip:SetHyperlink(link)
-		ShoppingTooltip1:SetOwner(GameTooltip, "ANCHOR_NONE");
-		ShoppingTooltip2:SetOwner(GameTooltip, "ANCHOR_NONE");
-		ShoppingTooltip3:SetOwner(GameTooltip, "ANCHOR_NONE");
+		if IsShiftKeyDown() then
+				
+			ShoppingTooltip1:SetOwner(GameTooltip, "ANCHOR_NONE");
+			ShoppingTooltip2:SetOwner(GameTooltip, "ANCHOR_NONE");
+			ShoppingTooltip3:SetOwner(GameTooltip, "ANCHOR_NONE");
 
-		local comp1 = nil
-		local comp2 = nil
-		local comp3 = nil
-		if ShoppingTooltip1:SetHyperlinkCompareItem(link,1) then
-			comp1 = true
-		end
-		if ShoppingTooltip2:SetHyperlinkCompareItem(link,2) then
-			comp2 = true
-		end
-		if ShoppingTooltip3:SetHyperlinkCompareItem(link,3) then
-			comp3 = true
-		end		
-		GameTooltip:Show()
-
-		local left, right, anchor1, anchor2 = GameTooltip:GetLeft(), GameTooltip:GetRight(), "TOPLEFT", "TOPRIGHT";
-		if comp1 then
-			ShoppingTooltip1:ClearAllPoints();
-			ShoppingTooltip1:SetPoint(anchor1, GameTooltip, anchor2, 0, -10);
-			ShoppingTooltip1:Show();
-		end
-		if comp2 then
-			ShoppingTooltip2:ClearAllPoints();
-			if comp1 then
-				ShoppingTooltip2:SetPoint(anchor1, ShoppingTooltip1, anchor2, 0, -10);
-			else
-				ShoppingTooltip2:SetPoint(anchor1, GameTooltip, anchor2, 0, -10);
+			local comp1 = nil
+			local comp2 = nil
+			local comp3 = nil
+			if ShoppingTooltip1:SetHyperlinkCompareItem(link,1,1,GameTooltip) then
+				comp1 = true
 			end
-			ShoppingTooltip2:Show();
-		end
-		if comp3 then
-			ShoppingTooltip3:ClearAllPoints();
-			ShoppingTooltip3:SetPoint(anchor1, GameTooltip, anchor2, 0, -10);
-			ShoppingTooltip3:Show();
-		end
+			if ShoppingTooltip2:SetHyperlinkCompareItem(link,2,1,GameTooltip) then
+				comp2 = true
+			end
+			if ShoppingTooltip3:SetHyperlinkCompareItem(link,3,1,GameTooltip) then
+				comp3 = true
+			end		
+			GameTooltip:Show()
 
+			local left, right, anchor1, anchor2 = GameTooltip:GetLeft(), GameTooltip:GetRight(), "TOPLEFT", "TOPRIGHT";
+			if comp1 then
+				ShoppingTooltip1:ClearAllPoints();
+				ShoppingTooltip1:SetPoint(anchor1, GameTooltip, anchor2, 0, -10);
+				ShoppingTooltip1:Show();
+			end
+			if comp2 then
+				ShoppingTooltip2:ClearAllPoints();
+				if comp1 then
+					ShoppingTooltip2:SetPoint(anchor1, ShoppingTooltip1, anchor2, 0, -10);
+				else
+					ShoppingTooltip2:SetPoint(anchor1, GameTooltip, anchor2, 0, -10);
+				end
+				ShoppingTooltip2:Show();
+			end
+			if comp3 then
+				ShoppingTooltip3:ClearAllPoints();
+				ShoppingTooltip3:SetPoint(anchor1, GameTooltip, anchor2, 0, -10);
+				ShoppingTooltip3:Show();
+			end
+		end
 	end
 end
 
@@ -326,7 +374,6 @@ function lola_texture_OnLeave(self, motion)
 end
 
 -- -----------------------------------------------------------------
-
 function lola_OnEvent(self, event, ...)
 	-- print(event)
 	if event == "PLAYER_ENTERING_WORLD" then
@@ -335,23 +382,31 @@ function lola_OnEvent(self, event, ...)
 		load_variables()
 	elseif event == "CHAT_MSG_LOOT" then
 		add_loot(...)
-	elseif event == "RAID_WARNING" then
+	elseif event == "CHAT_MSG_RAID_WARNING" then
 		warn_loot(...)
 	end
 end
 
 -----------------------------------------------------------------------
 local function lola_SlashCommand(msg, ...)
-	if LoLaMain:IsVisible() then
-		LoLaMain:Hide()
+	if string.sub(msg,1,7) =="addloot"then
+		local id = tonumber(string.sub(msg,8))
+		if id == nil then
+			debug("Gib doch bitte eine ID an!")
+		else
+			add_loot("item:" .. id .. ":")
+		end
 	else
-		LoLaMain:Show()
+		if LoLaMain:IsVisible() then
+			LoLaMain:Hide()
+		else
+			LoLaMain:Show()
+		end
 	end
 end
 -----------------------------------------------------------------------
 function lola_onLoad()
-	print("Loot Alert loaded")
-
+	print("|cff5555ffLoot Alert|r")	
 	
 	for i = 1, max_lines do
 		local wish = CreateFrame("Frame", "lolawish_" .. i, lola_wish_frame.main, "ListElementTemplate")
@@ -372,7 +427,7 @@ function lola_onLoad()
 	LoLaMain:RegisterEvent("PLAYER_ENTERING_WORLD")
 	LoLaMain:RegisterEvent("VARIABLES_LOADED")
 	LoLaMain:RegisterEvent("CHAT_MSG_LOOT")
-	LoLaMain:RegisterEvent("RAID_WARNING")
+	LoLaMain:RegisterEvent("CHAT_MSG_RAID_WARNING")
 	
 	SLASH_LOOTALERT1 = "/lola"
 	SlashCmdList["LOOTALERT"] = lola_SlashCommand
--- README.md
+++ README.md
@@ -1,27 +1,38 @@
 # LootAlert
-A simple wishlist WoW® 3.3.5a addon 
+A simple wishlist WoW© 3.3.5a addon 
 
 ## What is it good for
 This addon is a small list of items you may want to roll for when in a raid, e.g. ICC or Ruby. It's purpose is to hold track of what you want for your level 80 char.
 
 ![LootAlert](lootalert.png)
 
+<small>Artwork by Blizzard Entertainment, Inc®</small>
+
 The lower list shows the already looted items, so you can keep of track of items to be rolled for later.
 
-Future versions may show an alert, if a wished item was looted.
+If an item from the wishlist was looted, it is shown with a green background.
 
 Items are added by entering the item name or part of the item name into the input field in the left right corner, e.g. "Gruftm". You can use the item name in your WoW® language. It is recommendend, to click the "Scan" once, WoW® was started. This loads all epic+ items starting with id 40000 in an array. That allows to search for items, which have not yet seen by the char.
 
 If an item was not found, the entered item name is written into the textbox with a leading "nf " text.
 
-This little addon is still under devlopement, therefore, there is no zip file for download. But if it is cloned into `/Interface/Addons` directory, it can be used.
+If the mouse hovers over the item icon, the usual tooltip is shown. If the shift key was pressed while the mouse was moved over the item item, the usual tooltip and the compare tooltops (ShoppingToolTip) of currently equipped items for compare.
+
+If a raid warning contains the link of one of the items in the wishlist an alert sound is played. (`sound\\interface\\PVPFlagTakenHordeMono.wav` )
+
+(That was the idea behind "lootAlert".)
+
+This little addon is still under devlopement. 
 
 If the addon was closed, simply enter "/lola" in the chat to reopen the addon window.
 
-The addon was developed while playing on [Rising Gods](https://www.rising-gods.de/).
+The addon was developed while playing on [Rising Gods](https://www.rising-gods.de/), a free German 3.3.5a server.
 
 ## Installing
-Download LootAlert.zip. Change in `Interface/AddOns` directory and unip the files by keeping the file and directory structure. A new directory `LootAlert` is created with four new files in. 
+Download LootAlert.zip. Change in `Interface/AddOns` directory and unzip the files by keeping the file and directory structure. A new directory `LootAlert` is created with four new files in. 
 
 Use on your own risk.
+
+## Open issues
+When adding and item, it is not clear, whether it is heroic nor normal. Need to save both item Ids: normal and heroic. 
 
